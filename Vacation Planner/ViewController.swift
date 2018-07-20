16532454ab984a9928f9421ec92156681864f3f4
Yohan Berg <yohan520@gmail.com>
2018-07-20 09:31:48 -0700

    Manually merged planner-integration into master
    Merged planner-integration into master by hand. Also added some decor and connected the Attractions view 'createPlan' button to the planner view

--- Vacation Planner/ViewController.swift
+++ Vacation Planner/ViewController.swift
@@ -14,10 +14,12 @@ class ViewController: UIViewController {
     
     var expectedAttraction: Attraction?
 
+    @IBOutlet weak var plannerButton: UIButton!
     @IBOutlet weak var placesButton: UIButton!
     @IBAction func createPlan(segue: UIStoryboardSegue) {
         if expectedAttraction != nil {
             print("Segued back to main screen from \(expectedAttraction!.name)")
+            self.performSegue(withIdentifier: "createPlan", sender: self)
         } else {
             print("Segued back from unknown attraction")
         }
@@ -28,8 +30,17 @@ class ViewController: UIViewController {
         
         placesButton.layer.cornerRadius = 8.0
         placesButton.clipsToBounds = true
+        plannerButton.layer.cornerRadius = 8.0
+        plannerButton.clipsToBounds = true
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createPlan" {
+            let controller = segue.destination as! plannerController
+            controller.expectedAttraction = expectedAttraction
+        }
     }
 }
